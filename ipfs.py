206d0f209ef675883526b023958ba28a7fe48139
Sam <samyankun@gmail.com>
2018-08-07 23:53:43 +0800

    init commit

---
+++ ipfs.py
@@ -0,0 +1,24 @@
+import daemon
+import subprocess
+
+
+class Ipfs:
+    initCmdList = ['ipfs','init']
+    runCmdList = ['ipfs','daemon']
+    ManageConfigCmdList = ['ipfs','config','replace']
+    def __init__(self,bootstrap,storagemax,datadir):
+        self.bootstrap = bootstrap
+        self.storagemax = storagemax
+        self.datadir = datadir
+
+    def ipfsInit(self):
+        print('In ipfsInit')
+        subprocess.run(self.initCmdList)
+
+    def ipfsManageConfig(self,key,configValue):
+        subprocess.run(self.ManageConfigCmdList + ['key',configValue])
+
+    def ipfsDaemonRun(self):
+        log = open(("%s/%s" % (self.datadir,"ipfs.log")),'w+')
+        with daemon.DaemonContext(stdout=log,stderr=log):
+            subprocess.Popen(self.runCmdList)
---
+++ run.py
@@ -0,0 +1,48 @@
+from ipfs import Ipfs
+import argparse
+import json
+import configparser
+import os
+
+
+def addArgument(parser,argList):
+    print(argList)
+    parser.add_argument("command",help="start a bios node",choices=argList)
+    parser.add_argument("--key",help="the key for ipfs config")
+    parser.add_argument("--value",help="the value for key in ipfs config")
+
+
+def args_switch(ipfsInstance,args):
+    print('In args_switch()')
+    switcher = {
+    'init': ipfsInstance.ipfsInit,
+    'manageconfig': ipfsInstance.ipfsManageConfig,
+    'run': ipfsInstance.ipfsDaemonRun
+    }
+    if(args.command != 'manageconfig'):
+        switcher.get(args.command,'nothing')()
+    else:
+        switcher.get(args.command,'nothing')(args.key,args.value)
+
+
+
+
+def parseconfigfile():
+    config = configparser.ConfigParser()
+    dirname = os.path.dirname(__file__)
+    configfile = os.path.join(dirname, './config.ini')
+    config.read(configfile)
+    return config
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    addArgument(parser,['init','manageconfig','run'])
+    config = parseconfigfile()
+    args = parser.parse_args()
+    ipfsInstance = Ipfs(config['Init']['bootstrap'],config['Init']['storagemax'],config['Init']['datadir'])
+    args_switch(ipfsInstance,args)
+
+
+if __name__ == "__main__":
+    main()
